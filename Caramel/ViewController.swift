81430089e41b6f37a26babbbf8f5a0e6b4782460
greenac <andre1226.dev@gmail.com>
2016-06-13 08:56:53 -0700

    initial commit

---
+++ Caramel/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Caramel
+//
+//  Created by Andre Green on 6/13/16.
+//  Copyright © 2016 Sugar. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
